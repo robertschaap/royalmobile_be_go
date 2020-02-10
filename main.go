6d805d2861e5e322358071f210bc20dcb10691ca
robertschaap <robert.schaap@mac.com>
2020-02-10 21:19:30 +0100

    feat(main): setup skeleton

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+type ApiResponse struct {
+	Status  string      `json:"status"`
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+}
+
+func (r *ApiResponse) JSON(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(r)
+}
+
+func GetIndex(w http.ResponseWriter, r *http.Request) {
+	res := ApiResponse{"", "", ""}
+	res.JSON(w)
+}
+
+func GetProducts(w http.ResponseWriter, r *http.Request) {
+	res := ApiResponse{"", "", ""}
+	res.JSON(w)
+}
+
+func GetSubscriptions(w http.ResponseWriter, r *http.Request) {
+	res := ApiResponse{"", "", ""}
+	res.JSON(w)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	r := mux.NewRouter()
+	r.HandleFunc("/api/", GetIndex)
+	r.HandleFunc("/api/products", GetProducts)
+	r.HandleFunc("/api/subscriptions", GetSubscriptions)
+	http.ListenAndServe(":4000", r)
 }
