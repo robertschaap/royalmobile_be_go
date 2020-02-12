d0d96d0acd33290411bf29e7c3fe8107417fb2b5
robertschaap <robert.schaap@mac.com>
2020-02-12 21:30:00 +0100

    feat(index): setup index controller

---
+++ controllers/index.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/robertschaap/royalmobile_go_be/server"
+)
+
+// GetIndex returns basic information about the API when called
+func GetIndex(w http.ResponseWriter, r *http.Request) {
+	res := server.APIResponse{
+		Status:  "success",
+		Data:    "Hello World!",
+		Message: "Hello World!",
+	}
+
+	res.JSON(w)
+}
