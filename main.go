e9b0a38d680c82777be9b385c383865691d9522d
robertschaap <robert.schaap@mac.com>
2020-02-12 21:31:17 +0100

    refactor(main): import GetIndex from controllers

--- main.go
+++ main.go
@@ -5,21 +5,11 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/robertschaap/royalmobile_go_be/controllers"
-	"github.com/robertschaap/royalmobile_go_be/server"
 )
-
-func getIndex(w http.ResponseWriter, r *http.Request) {
-	res := server.APIResponse{
-		Status:  "",
-		Data:    "",
-		Message: "",
-	}
-	res.JSON(w)
-}
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/", getIndex)
+	r.HandleFunc("/api/", controllers.GetIndex)
 	r.HandleFunc("/api/products", controllers.GetProducts)
 	r.HandleFunc("/api/subscriptions", controllers.GetSubscriptions)
 	http.ListenAndServe(":4000", r)
