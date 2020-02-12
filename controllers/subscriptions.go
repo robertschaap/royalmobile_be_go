1efe881b7707b173530bd0c65e4148122578194e
robertschaap <robert.schaap@mac.com>
2020-02-12 21:24:36 +0100

    feat(subscriptions): setup basic products controller

---
+++ controllers/subscriptions.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/robertschaap/royalmobile_go_be/server"
+)
+
+// GetSubscriptions returns a list of all available subscriptions
+func GetSubscriptions(w http.ResponseWriter, r *http.Request) {
+	res := server.APIResponse{
+		Status:  "success",
+		Data:    "",
+		Message: "",
+	}
+}
