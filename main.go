1ac5ab93a20144dcda694ced529141f62ffd540e
robertschaap <robert.schaap@mac.com>
2020-02-16 13:29:57 +0100

    feat(main): add message showing server has started

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/robertschaap/royalmobile_go_be/router"
 )
 
 func main() {
+	fmt.Print("starting server")
 	r := router.CreateRouter()
 	http.ListenAndServe(":4000", r)
 }
