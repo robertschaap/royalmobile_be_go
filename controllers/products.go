029b1cee28548e74ba35676ce5910aee08cb3163
robertschaap <robert.schaap@mac.com>
2020-12-12 11:05:15 +0100

    feat(products): move disk access to model

--- controllers/products.go
+++ controllers/products.go
@@ -1,60 +1,17 @@
 package controllers
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	m "github.com/robertschaap/royalmobile_go_be/model"
 	"github.com/robertschaap/royalmobile_go_be/server"
 )
-
-// ProductVariant struct denotes a variation of the device with different color and/or capacity
-type ProductVariant struct {
-	ID              int32  `json:"id"`
-	VariantID       string `json:"variantId"`
-	Color           string `json:"color"`
-	ColorHex        string `json:"colorHex"`
-	Capacity        string `json:"capacity"`
-	IsInStock       bool   `json:"is_in_stock"`
-	IsPreorder      bool   `json:"is_preorder"`
-	RegularPrice    string `json:"regular_price"`
-	DiscountedPrice string `json:"discounted_price"`
-	HasDiscounts    bool   `json:"has_discounts"`
-}
-
-// Product struct denotes the base information of the device
-type Product struct {
-	ID           int32            `json:"id"`
-	Manufacturer string           `json:"manufacturer"`
-	Model        string           `json:"model"`
-	ModelID      string           `json:"modelId"`
-	Variants     []ProductVariant `json:"variants"`
-}
-
-func getProductsStubs() []Product {
-	data, err := ioutil.ReadFile("./stubs/products.json")
-
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	var products []Product
-
-	err = json.Unmarshal(data, &products)
-
-	if err != nil {
-		fmt.Println("error:", err)
-	}
-
-	return products
-}
 
 // GetProducts returns a list of all available phones and basic specifications.
 func GetProducts(w http.ResponseWriter, r *http.Request) {
 	res := server.APIResponse{}
-	res.Success(getProductsStubs())
+	res.Success(m.GetProducts())
 	res.JSON(w)
 }
 
@@ -62,9 +19,9 @@ func GetProducts(w http.ResponseWriter, r *http.Request) {
 func GetProduct(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 
-	var product Product
+	var product m.Product
 
-	for _, v := range getProductsStubs() {
+	for _, v := range m.GetProducts() {
 		if v.ModelID == id {
 			product = v
 			break
---
+++ models/products.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+// ProductVariant struct is a struct
+type ProductVariant struct {
+	ID              int32  `json:"id"`
+	VariantID       string `json:"variantId"`
+	Color           string `json:"color"`
+	ColorHex        string `json:"colorHex"`
+	Capacity        string `json:"capacity"`
+	IsInStock       bool   `json:"is_in_stock"`
+	IsPreorder      bool   `json:"is_preorder"`
+	RegularPrice    string `json:"regular_price"`
+	DiscountedPrice string `json:"discounted_price"`
+	HasDiscounts    bool   `json:"has_discounts"`
+}
+
+// Product struct denotes the base information of the device
+type Product struct {
+	ID           int32            `json:"id"`
+	Manufacturer string           `json:"manufacturer"`
+	Model        string           `json:"model"`
+	ModelID      string           `json:"modelId"`
+	Variants     []ProductVariant `json:"variants"`
+}
+
+// GetProducts reads products from disk or returns an empty slice
+func GetProducts() []Product {
+	data, err := ioutil.ReadFile("./stubs/products.json")
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	var products []Product
+
+	err = json.Unmarshal(data, &products)
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	return products
+}
