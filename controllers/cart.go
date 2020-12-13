c9428684d8b9bfe37d3bf6bf3e04fbb258891e31
robertschaap <robert.schaap@mac.com>
2020-12-13 12:50:39 +0100

    feat(cart): add comments in controller

--- controllers/cart.go
+++ controllers/cart.go
@@ -3,31 +3,32 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/robertschaap/royalmobile_go_be/models"
 	"github.com/robertschaap/royalmobile_go_be/server"
 )
 
-// GetCart returns a shopping cart with products
+// GetCart takes a UUIDv4 string "cartID" and returns a Cart or error
 func GetCart(w http.ResponseWriter, r *http.Request) {
 	res := server.APIResponse{}
-	res.Success(nil)
+	res.Success(models.GetCartByID("new-cart"))
 	res.JSON(w)
 }
 
-// AddCartItem returns a shopping cart with products
+// AddCartItem takes a UUIDv4 string "cartID" or the keyword "new" and returns a Cart or error
 func AddCartItem(w http.ResponseWriter, r *http.Request) {
 	res := server.APIResponse{}
 	res.Success(nil)
 	res.JSON(w)
 }
 
-// DeleteCartItem returns a shopping cart with products
+// DeleteCartItem takes a UUIDv4 string "cartID" and "itemID" and returns a Cart without the item to delete or error
 func DeleteCartItem(w http.ResponseWriter, r *http.Request) {
 	res := server.APIResponse{}
 	res.Success(nil)
 	res.JSON(w)
 }
 
-// PostOrder returns a shopping cart with products
+// PostOrder takes a UUIDv4 string "cartID" and returns a Cart if succesful or an error
 func PostOrder(w http.ResponseWriter, r *http.Request) {
 	res := server.APIResponse{}
 	res.Success(nil)
